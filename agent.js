2291a8f4223c385277d5891452c925847d293de3
NadetDev <nadetdev@gmail.com>
2025-12-14 20:37:12 +0100

    with langChain

--- agent.js
+++ agent.js
@@ -4,12 +4,9 @@ dotenv.config();
 
 const client = new OpenAI();
 
-    const response = await client.responses.create({
-  model: "gpt-4.1-nano",
+const response = await client.responses.create({
+  model: "gpt-4o-mini",
   input: "Quelle est la capitale de la République du Congo ?",
 });
 
 console.log(response.output_text);
-
-
-
